d2747f2cc959b1517de3ed48b9ba8ed2ac5137f9
vbhayden <33755714+vbhayden@users.noreply.github.com>
2018-06-26 12:26:41 -0400

    Update activity.js

--- dist/schemaRules/activity.js
+++ dist/schemaRules/activity.js
@@ -5,7 +5,7 @@ var factory_1 = require("../factory");
 var restrictToValue_1 = require("../helpers/restrictToValue");
 exports.default = rulr_1.restrictToSchema({
     objectType: rulr_1.optional(restrictToValue_1.default('Activity')),
-    id: rulr_1.optional(factory_1.iri),
+    id: rulr_1.required(factory_1.stringValue),
     definition: rulr_1.optional(factory_1.definition),
 });
 //# sourceMappingURL=activity.js.map
